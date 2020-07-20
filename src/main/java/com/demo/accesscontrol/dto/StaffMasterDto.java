9568ba91f86169c1cef0c91cf5c534b5addf0fe8
Tushar mahajan <mahajan.tushar31@gmail.com>
2020-07-20 10:03:02 +0530

    getters setter added

--- src/main/java/com/demo/accesscontrol/dto/StaffMasterDto.java
+++ src/main/java/com/demo/accesscontrol/dto/StaffMasterDto.java
@@ -5,13 +5,6 @@ History:
 Date:Jul 18, 2020:created
 ============================*/
 
-/*===========================
-FileName: StaffMasterDto.java
-Author:Tushar Mahajan
-History:
-Date:Jul 18, 2020:created
-============================*/
-
 package com.demo.accesscontrol.dto;
 
 import java.util.Date;
@@ -28,35 +21,102 @@ public class StaffMasterDto {
 	/*
 	 * @Getter @Setter private Integer staff_id; //20
 	 */	
-	
-	@Getter @Setter
+	// firstname lastname dob email phone date_of_joining last_active_date
     private String firstname;
 
-	@Getter @Setter
     private String lastname;	// varchar2	50
 	
-	@Getter @Setter
     private Date dob;	
 	
-	@Getter @Setter
     private String email;  //	varchar2	50
 	
-	@Getter @Setter
     private String phone;	//varchar2	20
 	
-	@Getter @Setter
     private Date date_of_joining;
 	
-	@Getter @Setter
     private Date last_active_date; //	date	
 
+	
+
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+	public Date getDob() {
+		return dob;
+	}
+
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public Date getDate_of_joining() {
+		return date_of_joining;
+	}
+
+
+	public void setDate_of_joining(Date date_of_joining) {
+		this.date_of_joining = date_of_joining;
+	}
+
+
+	public Date getLast_active_date() {
+		return last_active_date;
+	}
+
+
+	public void setLast_active_date(Date last_active_date) {
+		this.last_active_date = last_active_date;
+	}
 	
 	@Override
 	public String toString() {
 		return "StaffMasterDto [firstname=" + firstname + ", lastname=" + lastname + ", dob=" + dob + ", email=" + email
 				+ ", phone=" + phone + ", date_of_joining=" + date_of_joining + ", last_active_date=" + last_active_date
-				+ ", createdAt=" + "]";
+				+ "]";
 	}
+
+	
+	
 	
 
 }
