357e4c84d78e3d2a8b736480a457eededcfea206
Tushar mahajan <mahajan.tushar31@gmail.com>
2020-07-22 21:48:27 +0530

    project constants

---
+++ src/main/java/com/demo/accesscontrol/common/util/AccessCtrlConstant.java
@@ -0,0 +1,25 @@
+/*===========================
+FileName: AcccessControlConstant.java
+Author:Tushar Mahajan
+History:
+Date:Jul 20, 2020:created
+============================*/
+
+package com.demo.accesscontrol.common.util;
+
+/**
+ * @author Tushar mahajan
+ *
+ */
+public final class AccessCtrlConstant {
+public static final String AUTH_EMAIL="authEmail";
+public static final String AUTH_MAIL_PASS="authEmailPass";
+public static final String EMAIL_SUBJECT="emailSubject";
+public static final String EMAIL_CONTENT="emailContent";
+public static final String EMAIL_FROM="emailFrom";
+public static final String RECEIVE_EMAIL="receivingEmail";
+
+public static final String ATTACHEMENT="attachement";
+public static final String MIME_MSG_CONTENT="mimeMsgContent";
+
+}
