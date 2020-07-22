fac0fd8a889383bed21324bb7addcf6758a7f10b
Tushar mahajan <mahajan.tushar31@gmail.com>
2020-07-22 22:15:56 +0530

    updated with new fields

--- src/main/java/com/demo/accesscontrol/entity/UserAccess.java
+++ src/main/java/com/demo/accesscontrol/entity/UserAccess.java
@@ -1,8 +1,10 @@
 /*===========================
 FileName: AccessTab.java
+
 Author:Tushar Mahajan
 History:
 Date:Jul 17, 2020:created
+	:Jul 20, 2020:1:table structure updated acce_name=email,accessCode="ACC1",QR_code=""	
 ============================*/
 
 /**
@@ -19,7 +21,6 @@ import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
@@ -30,42 +31,50 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * @author Tushar mahajan
  *
  */
 @Entity
-@Table(name = "access_master")
+// @Table(name = "access_master")
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
-public class AccessMaster implements Serializable{
+public class UserAccess implements Serializable{
+// old 
+// 	staff_id,  password, role_id, access_creation_date, access_valid_date
+	
+//  new unsestanding	
+// 	User,    QR(AccCode),role_id, access_creation_date,access_valid_date	
 
 	@Id
-	@Getter @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="staff_id")
-	private Integer  staff_id;	//	number	20
+	private Integer  access_id;	
+
+	@Column(length=45,nullable = false,unique=true)
+	private String  access_name;	
 	
-	@Column(name = "password",nullable = false)
+	@Column(length=25,nullable = false)
+	private String  access_code;
+	
+	@Column(length=20,nullable = false)
 	@NotNull
-    private String password;	//	varchar2	30
+    private String role_id;		
 	
-	@Column(name = "role_id",nullable = false)
-	@NotNull
-    private String role_id;		//	number	20
+	/*
+	 * @Column(length=20,nullable = false)
+	 * 
+	 * @NotNull private String qr_code;
+	 */
 	
-	@Column(name = "access_creation_date",nullable = false)
+	@Column(nullable = false)
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
-    private Date accessCreationDate;	//	date	
+    private Date accessCreationDate;	
 	
 	@Column(name = "access_valid_date",nullable = false)
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
-    private Date accessValidDate;	//	date	
+    private Date accessValidDate;		
 	
 
 	// ----- For Record ---------
@@ -78,33 +87,43 @@ public class AccessMaster implements Serializable{
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
     private Date updatedAt;
+    
 
     //-----------------GETTERS / SETTER STARTS -------------------
     
-
-	public Integer getStaff_id() {
-		return staff_id;
+    public Integer getAccess_id() {
+		return access_id;
 	}
 
-	public void setStaff_id(Integer staff_id) {
-		this.staff_id = staff_id;
+	public void setAccess_id(Integer access_id) {
+		this.access_id = access_id;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getAccess_name() {
+		return access_name;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setAccess_name(String access_name) {
+		this.access_name = access_name;
+	}
+
+	public String getAccess_code() {
+		return access_code;
+	}
+
+	public void setAccess_code(String access_code) {
+		this.access_code = access_code;
 	}
 
 	public String getRole_id() {
 		return role_id;
 	}
 
+
 	public void setRole_id(String role_id) {
 		this.role_id = role_id;
 	}
+
 
 	public Date getAccessCreationDate() {
 		return accessCreationDate;
@@ -134,21 +153,18 @@ public class AccessMaster implements Serializable{
 		return updatedAt;
 	}
 
+
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
 
-	
-    
-    
-    
-    // ----------------- GETTER /SETTER ENDS -------------------- 
 	@Override
 	public String toString() {
-		return "AccessTab [staff_id=" + staff_id + ", password=" + password + ", role_id=" + role_id
-				+ ", accessCreationDate=" + accessCreationDate + ", accessValidDate=" + accessValidDate + ", createdAt="
-				+ createdAt + ", updatedAt=" + updatedAt + "]";
+		return "AccessMaster [access_id=" + access_id + ", access_name=" + access_name + ", access_code=" + access_code
+				+ ", role_id=" + role_id + /* ", qr_code=" + qr_code + */  ", accessCreationDate="+ accessCreationDate
+				+ ", accessValidDate=" + accessValidDate + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt
+				+ "]";
 	}
 
-    
+
 }
