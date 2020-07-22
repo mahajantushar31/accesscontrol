c76504b896860df4f80ac08a59e8a7eb5655c4e4
Tushar mahajan <mahajan.tushar31@gmail.com>
2020-07-22 22:22:32 +0530

    update fields name of entity

--- src/main/java/com/demo/accesscontrol/entity/User.java
+++ src/main/java/com/demo/accesscontrol/entity/User.java
@@ -19,6 +19,9 @@ import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -38,118 +41,153 @@ import lombok.Setter;
  *
  */
 @Entity
-@Table(name = "staff_master")
+// @Table(name = "staff_master")
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
-public class StaffMaster implements Serializable {
+public class User implements Serializable {
+	
+// old: // staff_id firstname lastname dob email phone date_of_joining last_active_date
+	
+// new:	email	Role-_id Password	firstname	lastname	dob	phone	date_of_joining	last_active_date	
+
 	
 	@Id
-	@Getter @Setter											// staff_id firstname lastname dob email phone date_of_joining last_active_date
+	@Getter @Setter											
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="staff_id")
-	private Integer  staff_id; //20
+	@Column(length=20)
+	private long  user_id; 
+	// @OneToOne(optional=false)  @JoinColumn(  name="", unique=true, nullable=false, updatable=false)
+	@Column(nullable = false,length=50,unique=true)@NotNull
+    private String email;  
 	
-	@Column(name = "first_name",nullable = false)
+	@Column(length = 15,nullable = true)
 	@NotNull
-	@Getter @Setter
+    private String role_id;
+	
+	@Column(length = 25,nullable = false)
+	@NotNull
+    private String password;
+	
+	@Column(length = 25,nullable = false)
+	@NotNull
     private String firstname;
 	
-	@Column(name = "last_name",nullable = true)
+	@Column(length = 25,nullable = true)
 	@NotNull
-	@Getter @Setter
     private String lastname;	// varchar2	50
 	
-	@Column(name = "dob",nullable = true)
+	@Column(insertable=false,nullable = true, updatable = false)
 	@NotNull
-	@Getter @Setter
-    private Date dob;	
+	//@Temporal(TemporalType.TIMESTAMP)
+    private Date dob;
 	
-	@Column(name = "email",nullable = true)
-	@NotNull
-	@Getter @Setter
-    private String email;  //	varchar2	50
-	
-	@Column(name = "phone",nullable = true)
-	@Getter @Setter
+	@Column(length = 20,nullable = true)
     private String phone;	//varchar2	20
 	
-	@Column(name = "date_of_joining",nullable = true)
+	@Column(nullable = true)
 	@Temporal(TemporalType.TIMESTAMP)
-	@Getter @Setter
     private Date date_of_joining;
 	
-	@Column(name = "last_active_date", insertable=false,nullable = true, updatable = false)
+	@Column(insertable=false,nullable = true, updatable = false)
 	@Temporal(TemporalType.TIMESTAMP)
-	@Getter @Setter
     private Date last_active_date; //	date	
 
-	// --------------GETTERS SETTERS -----------------
+
+	// ----- For Record ---------
+	/*
+	 * @Column(nullable = false, updatable = false)
+	 * 
+	 * @Temporal(TemporalType.TIMESTAMP)
+	 * 
+	 * @CreatedDate private Date createdAt;
+	 * 
+	 * 
+	 * 
+	 * @Column(nullable = false)
+	 * 
+	 * @Temporal(TemporalType.TIMESTAMP)
+	 * 
+	 * @LastModifiedDate private Date updatedAt;
+	 */
+	// CTOR
+	public User(){}
+	
+	public User(String roleId){
+		this.role_id=roleId;
+	}
+	
+	
 	
 
-    public Integer getStaff_id() {
-		return staff_id;
+	// --------------GETTERS SETTERS -----------------
+
+	public long getUser_id() {
+		return user_id;
 	}
 
-
-	public void setStaff_id(Integer staff_id) {
-		this.staff_id = staff_id;
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
 	}
-
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-
-	public String getLastname() {
-		return lastname;
-	}
-
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-
-	public Date getDob() {
-		return dob;
-	}
-
-
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	public String getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(String role_id) {
+		this.role_id = role_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
 
 	public String getPhone() {
 		return phone;
 	}
 
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-
 	public Date getDate_of_joining() {
 		return date_of_joining;
 	}
-
 
 	public void setDate_of_joining(Date date_of_joining) {
 		this.date_of_joining = date_of_joining;
@@ -160,64 +198,30 @@ public class StaffMaster implements Serializable {
 		return last_active_date;
 	}
 
-
 	public void setLast_active_date(Date last_active_date) {
 		this.last_active_date = last_active_date;
 	}
 
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
+	/*
+	 * public Date getCreatedAt() { return createdAt; }
+	 * 
+	 * public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
+	 * 
+	 * 
+	 * public Date getUpdatedAt() { return updatedAt; }
+	 * 
+	 * 
+	 * public void setUpdatedAt(Date updatedAt) { this.updatedAt = updatedAt; }
+	 */
 
 
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	// --------------GETTERS SETTERS -----------------
-	
-	// ----- For Record ---------
-	@Column(nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @Getter @Setter
-    @CreatedDate
-    private Date createdAt;
-
-
-
-	@Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    @Getter @Setter
-    private Date updatedAt;
-
-    
 	@Override
 	public String toString() {
-		return "StaffMasterTab [staff_id=" + staff_id + ", firstname=" + firstname + ", lastname=" + lastname + ", dob="
-				+ dob + ", email=" + email + ", phone=" + phone + ", date_of_joining=" + date_of_joining
-				+ ", last_active_date=" + last_active_date + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt
-				+ "]";
+		return "User [user_id=" + user_id + ", email=" + email + ", role_id=" + role_id + ", password="
+				+ password + ", firstname=" + firstname + ", lastname=" + lastname + ", dob=" + dob + ", phone=" + phone
+				+ ", date_of_joining=" + date_of_joining + ", last_active_date=" + last_active_date + ""
+						+ //", createdAt="+ createdAt + ", updatedAt=" + updatedAt +
+						"]";
 	}
 
 
